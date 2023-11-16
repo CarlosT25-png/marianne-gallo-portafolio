299bf47b95e49fdb72c2523f66a5b72b070c64e2
CarlosT25-png <cartorresva@gmail.com>
2023-11-15 23:01:28 -0600

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,14 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
-  title: "Marianne Gallo",
+  title: "Marianne Gallo | UX/UI Designer",
+  openGraph: {
+    type: 'website',
+    title: 'Marianne Gallo | UX/UI Designer',
+    url: 'https://mariannegallo.com',
+    description: 'Marianne Gallo - Aspiring UX/UI designer crafting exceptional user experiences. Explore my portfolio of passion, creativity, and user-centric designs.'
+  },
+  keywords: 'UX/UI design, web design, graphic design, user experience, user interface, portfolio, designer, creative, passionate, dedicated',
   description:
     "Embark on a journey of design excellence with Marianne Gallo, an aspiring UX/UI designer dedicated to continuous learning and exceptional experiences. Explore a portfolio that reflects passion, creativity, and a commitment to crafting user-centric designs.",
 };

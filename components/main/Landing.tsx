2243062f3275d05145efdb5e90d3db2364abd989
CarlosT25-png <cartorresva@gmail.com>
2023-11-15 22:33:15 -0600

    landing animation finish

--- components/main/Landing.tsx
+++ components/main/Landing.tsx
@@ -6,19 +6,41 @@ const poppins = Poppins({ subsets: ["latin"], weight: "700" });
 const Landing = () => {
   return (
     <>
-      <img src="/images/landing/Background.gif" alt="Background gif"  className={styles['bg-image']}/>
-      <div className={styles['title-container']}>
-        <div className={styles['title-container__flex']}>
-          <div className={`${poppins.className} ${styles['words']}`}>
-            <span>Welcome</span>
-            <span>Bienvenido</span>
-            <span>Bienvenue</span>
-            <span>ようこそ</span>
-            <span>स्वागत है</span>
-          </div>
-          <h1 className={`${poppins.className} ${styles['title-container__static-text']} ${styles['title-container__static-text-1'] }`}> to my</h1>
+      <img
+        src="/images/landing/Background.gif"
+        alt="Background gif"
+        className={styles["bg-image"]}
+      />
+      <div className={styles["title-container"]}>
+        <div className={styles["title-container__flex"]}>
+          <h1
+            className={`${poppins.className} ${styles["title-container__static-text"]}`}
+          >
+            <div className={`${styles["inner-headings"]}`}>
+                <span className={`${styles["celestin"]}`}>
+                  Welcome <br />
+                </span>
+                <span className={`${styles["naranjin"]}`}>
+                  Bienvenido <br />
+                </span>
+                <span className={`${styles["rosadin"]}`}>
+                  Bienvenue <br />
+                </span>
+                <span className={`${styles["moradin"]}`}>
+                  ようこそ <br />
+                </span>
+                <span className={`${styles["verdin"]}`}>
+                  स्वागत <br />
+                </span>
+            </div>
+          </h1>
+          <h1>to my</h1>
         </div>
-        <h1 className={`${poppins.className} ${styles['title-container__static-text']}`}>Creative Space</h1>
+        <h1
+          className={`${poppins.className} ${styles["title-container__static-text"]} ${styles["bottom-align"]}`}
+        >
+          Creative Space
+        </h1>
       </div>
     </>
   );

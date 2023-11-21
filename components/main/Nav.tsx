7131f216aa57b8fc689c89eac1544393dbddf28e
CarlosT25-png <cartorresva@gmail.com>
2023-11-20 22:05:57 -0600

    new nav menu

--- components/main/Nav.tsx
+++ components/main/Nav.tsx
@@ -1,31 +1,56 @@
-import { Poppins } from 'next/font/google';
-import styles from './Nav.module.css'
+import { Poppins } from "next/font/google";
+import { IoMenu, IoClose } from "react-icons/io5";
 
-const poppins = Poppins({ weight: '700', subsets: ['latin']})
+import styles from "./Nav.module.css";
+import { useState } from "react";
+
+const poppins = Poppins({ weight: "700", subsets: ["latin"] });
 
 type Props = {
-  name: string,
-  url: string,
-  color: string
-}
+  name: string;
+  url: string;
+  color: string;
+};
 
 const Link = (props: Props) => {
-  return <li className={`${styles.link} ${props.color}`}>
-    <a href={props.url}>{props.name}</a>
-  </li>
-}
+  return (
+    <li className={`${styles.link} ${props.color}`}>
+      <a href={props.url}>{props.name}</a>
+    </li>
+  );
+};
 
-const Nav = () => {
+const InternalMenu = () => {
   return (
     <nav className={`${styles.nav} ${poppins.className}`}>
       <ul className={`${styles.list}`}>
-        <Link name='Home' url='#home' color={styles.celestin}/>
-        <Link name='About' url='' color={styles.naranjin}/>
-        <Link name='Work' url='#work' color={styles.rosadin}/>
-        <Link name='Contact' url='' color={styles.moradin}/>
+        <Link name="Home" url="#home" color={styles.celestin} />
+        <Link name="About" url="" color={styles.naranjin} />
+        <Link name="Work" url="#work" color={styles.rosadin} />
+        <Link name="Contact" url="" color={styles.moradin} />
       </ul>
     </nav>
   );
-}
+};
+
+const Nav = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  return (
+    <>
+      <button type="button" className={`${styles["menu-btn"]}`} onClick={() => setShowMenu(val => !val)}>
+        {!showMenu && (
+          <IoMenu />
+        )}
+        {showMenu && (
+          <IoClose />
+        )}
+      </button>
+      {showMenu && (
+        <InternalMenu />
+      )}
+    </>
+  );
+};
 
 export default Nav;
